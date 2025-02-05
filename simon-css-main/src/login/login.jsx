294afb3d9e6c389a8949b8e538d42c8c446a6113
Ashton <thejagar22@gmail.com>
2025-02-05 16:54:15 -0700

    -Updated all pages and JSX to display main contents.

--- simon-css-main/src/login/login.jsx
+++ simon-css-main/src/login/login.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Login() {
   return (
 <main className="container-fluid bg-secondary text-center">
         <div>
           <h1>Welcome to Simon</h1>
-          <form method="get" action="play.html">
             <div className="input-group mb-3">
               <span className="input-group-text">@</span>
               <input className="form-control" type="text" placeholder="your@email.com" />
@@ -14,9 +14,8 @@ export function Login() {
               <span className="input-group-text">🔒</span>
               <input className="form-control" type="password" placeholder="password" />
             </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-            <button type="submit" className="btn btn-secondary">Create</button>
-          </form>
+            <NavLink to="/play" className="btn btn-primary">Login</NavLink>
+            <NavLink to="/play" className="btn btn-secondary">Create</NavLink>
         </div>
       </main>
   );
