ce46f4e0927669d6aa87674e8245957621c8a897
Ashton <thejagar22@gmail.com>
2025-02-06 17:59:27 -0700

    -Temporarily got all the routing done. Need to move app contents inside now.

--- html/src/app.jsx
+++ html/src/app.jsx
@@ -1,38 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Chat } from './chat/chat';
+import { About } from './about/about';
 
 export default function App() {
-  return (
-  <><header>
-          <h1>JagarChat</h1>
-          <nav className="navbar">
-              <menu className="nav-link">
-                  <li className="nav-item">
-                      <a className="nav-link active" href="index.html">
-                          Home
-                      </a>
-                  </li>
-                  <li>
-                      <a className="nav-link" href="chat.html">
-                          Chat
-                      </a>
-                  </li>
-                  <li>
-                      <a className="nav-link" href="about.html">
-                          About
-                      </a>
-                  </li>
-              </menu>
-          </nav>
-      </header><main>jsadjkoaj</main><footer className="bg-dark text-white">
-              <div className="container-fluid d-flex justify-content-between align-items-center">
-                  <span className="text-reset">Author Name(s)</span>
-                  <br />
-                  <a className="text-reset" href="https://github.com/JagarJaguar/startup">
-                      GitHub
-                  </a>
-              </div>
-          </footer></>
-  );
+    return (
+        <BrowserRouter>
+            <>
+            <body>
+            <header>
+                <h1>JagarChat</h1>
+                <nav className="navbar">
+                    <menu className="nav-link">
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to=''>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className='nav-link' to='chat'>Chat</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className='nav-link' to='about'>About</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/chat' element={<Chat />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+
+                <footer className="bg-dark text-white">
+                    <div className="container-fluid d-flex justify-content-between align-items-center">
+                        <span className="text-reset">Author Name(s)</span>
+                        <br />
+                        <a className="text-reset" href="https://github.com/JagarJaguar/startup">
+                            GitHub
+                        </a>
+                    </div>
+                </footer>
+                </body></>
+        </BrowserRouter>
+    );
+}
+
+function NotFound() {
+    return <main>404: Return to sender. Address unknown.</main>;
 }
