bebded0cdbd9bd6a7bc3a5981eb1ece38adb74a6
Ashton <thejagar22@gmail.com>
2025-02-06 18:14:18 -0700

    -Moved chat HTML to chat.jsx. Need to fix some stuff with the CSS/html.

--- html/src/chat/chat.jsx
+++ html/src/chat/chat.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import './chat.css';
 
 export function Chat() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main>
+    <h2>Start Chatting Now!</h2>
+    <label for="textarea"><b>Logged in as: </b> Username</label>
+    <br />
+    <textarea id="textarea" name="varTextarea" className="form-control"
+        rows="14" cols="60"
+        readonly>
+
+
+
+
+
+
+
+
+
+
+
+Doc: How is HTML going so far?
+Melissa: Hey guys!
+Bob: Hi 😄
+    </textarea>
+    <br />
+
+    <form method="get" action="chat.html">
+        <div className="input-group">
+            <input className="form-control" type="text"
+                placeholder="Your message here" />
+            <button type="button" class="btn btn-primary">😀</button>
+            <button type="submit" class="btn btn-secondary">Send
+                Message</button>
+        </div>
+    </form>
     </main>
   );
 }
