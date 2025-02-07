617ecd42c9726a3a3f0b62ea1e5dfb0248413725
Ashton <thejagar22@gmail.com>
2025-02-06 19:01:35 -0700

    -Added classes in the chat.jsx file for each of the elements so that way it doesn't interact with app.jsx at all. -Changed a bit of the CSS to work with the page, need to style a little more to get it to look a bit better. Similar to old chat page so far.

--- html/src/chat/chat.jsx
+++ html/src/chat/chat.jsx
@@ -2,40 +2,27 @@ import React from 'react';
 import './chat.css';
 
 export function Chat() {
-  return (
-    <main>
-    <h2>Start Chatting Now!</h2>
-    <label for="textarea"><b>Logged in as: </b> Username</label>
-    <br />
-    <textarea id="textarea" name="varTextarea" className="form-control"
-        rows="14" cols="60"
-        readonly>
+    return (
+        <main className="chat-page">
+            <div className="chat-container">
+                <h2>Start Chatting Now!</h2>
+                <label htmlFor="textarea"><b>Logged in as: </b> Username</label>
+                <br />
+                <textarea id="textarea" name="varTextarea" className="form-control" rows="14" cols="60"readOnly>
+                    Melissa: How is HTML going so far?
+                </textarea>
+                <br />
 
-
-
-
-
-
-
-
-
-
-
-Doc: How is HTML going so far? 
-Melissa: Hey guys!
-Bob: Hi 😄
-    </textarea>
-    <br />
-
-    <form method="get" action="chat.html" className="textForm">
-        <div className="input-group">
-            <input className="form-control" type="text"
-                placeholder="Your message here" />
-            <button type="button" class="btn btn-primary">😀</button>
-            <button type="submit" class="btn btn-secondary">Send
-                Message</button>
-        </div>
-    </form>
-    </main>
-  );
+                <form method="get" action="chat.html" className="textForm">
+                    <div className="chat-input-group">
+                        <input className="form-control" type="text"
+                            placeholder="Your message here" />
+                        <button type="button" className="btn btn-primary">😀</button>
+                        <button type="button" className="btn btn-secondary">Send
+                            Message</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
 }
