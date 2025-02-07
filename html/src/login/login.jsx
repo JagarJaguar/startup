b6d5cbd38790071b01fd4a194ac2c2ea887b9dcc
Ashton <thejagar22@gmail.com>
2025-02-06 18:04:34 -0700

    -Moved about.css to about folder -Fixed CSS styling button spacing. For some reason some CSS elements are not carrying over.

--- html/src/login/login.jsx
+++ html/src/login/login.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
-    </main>
+    <main>
+    <div>
+        <h1><b>Welcome to JagarChat!</b></h1>
+        <form method="get" action="chat.html">
+            <div className="input-group">
+                <span className="input-group-text">👤</span>
+                <input className="form-control" type="text"
+                    placeholder="Enter Username" />
+            </div>
+            <div className="input-group">
+                <span className="input-group-text">🔒</span>
+                <input className="form-control" type="password"
+                    placeholder="Enter Password" />
+            </div>
+            <button type="submit"
+                className="btn btn-secondary">Login</button>
+            <button type="submit" class="btn btn-primary">Create
+            </button>
+        </form>
+    </div>
+    <br />
+</main>
   );
 }
