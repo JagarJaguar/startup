c0aacf011de83be91a9d7f01542618cb7a4235cb
Ashton <thejagar22@gmail.com>
2025-02-15 11:17:49 -0700

    -Updated About page for react. Created useEffect hook for random emojis and the image URL (Image URL was to practice). -Emojis updated randomly when page refreshes, placeholder for 3rd party API that has a database of emojis to choose from.

--- html/src/about/about.jsx
+++ html/src/about/about.jsx
@@ -3,10 +3,20 @@ import './about.css';
 
 
 export function About() {
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [randEmoji, setEmoji] = React.useState('😜');
+
+  React.useEffect(() => {
+    setImageUrl('about_image.jpg')
+
+    const emojis = ['🔥', '🚀', '🎉', '😎', '🤖', '🌟', '🐱', '🍕'];
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    setEmoji(emojis[randomIndex]);
+  },[]);
   return (
 <main>
   <div id="picture" className="picture-box">
-    <img src="about_image.jpg" />
+    <img src={imageUrl} />
   </div>
   <p className="caption" style={{ marginTop: 0 }}>
     <i>Photo by me. I like my car.</i>
@@ -15,7 +25,7 @@ export function About() {
     <b>JagarChat</b> allows users to communicate with each other in{" "}
     <b>real time</b>. Users are able to login using a secure database.
   </p>
-  <div id="emoji">😜</div>
+  <div id="emoji" alt='random-emoji'>{randEmoji}</div>
   <br />
 </main>
   );
