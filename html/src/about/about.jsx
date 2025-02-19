8d698f0b6436f804c1c37dd06f976bfa09fd0a51
Ashton <thejagar22@gmail.com>
2025-02-19 15:48:21 -0700

    -Formatted.

--- html/src/about/about.jsx
+++ html/src/about/about.jsx
@@ -12,21 +12,21 @@ export function About() {
     const emojis = ['🔥', '🚀', '🎉', '😎', '🤖', '🌟', '🐱', '🍕'];
     const randomIndex = Math.floor(Math.random() * emojis.length);
     setEmoji(emojis[randomIndex]);
-  },[]);
+  }, []);
   return (
-<main>
-  <div id="picture" className="picture-box">
-    <img src={imageUrl} />
-  </div>
-  <p className="caption" style={{ marginTop: 0 }}>
-    <i>Photo by me. I like my car.</i>
-  </p>
-  <p>
-    <b>JagarChat</b> allows users to communicate with each other in{" "}
-    <b>real time</b>. Users are able to login using a secure database.
-  </p>
-  <div id="emoji" alt='random-emoji'>{randEmoji}</div>
-  <br />
-</main>
+    <main>
+      <div id="picture" className="picture-box">
+        <img src={imageUrl} />
+      </div>
+      <p className="caption" style={{ marginTop: 0 }}>
+        <i>Photo by me. I like my car.</i>
+      </p>
+      <p>
+        <b>JagarChat</b> allows users to communicate with each other in{" "}
+        <b>real time</b>. Users are able to login using a secure database.
+      </p>
+      <div id="emoji" alt='random-emoji'>{randEmoji}</div>
+      <br />
+    </main>
   );
 }
