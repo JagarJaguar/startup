1437f5be1d538cc2547f8577919135e225f9bf1c
Ashton <thejagar22@gmail.com>
2025-02-21 00:28:17 -0700

    -Reverted back to basics, need to maybe add some stuff in app.js to get it working.

--- html/src/login/login.jsx
+++ html/src/login/login.jsx
@@ -1,57 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from "react-router-dom";
 
 export function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
     const isLoggedIn = localStorage.getItem("isAuthenticated") === "true";
-    const storedUsers = JSON.parse(localStorage.getItem("users")) || {};
+    // const storedUsers = JSON.parse(localStorage.getItem("users")) || {};
 
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    if (storedUsers[username] && storedUsers[username] === password) {
-      localStorage.setItem("isAuthenticated", "true");
-      localStorage.setItem("currentUser", username);
-    }
-  };
+    const handleLogin = (e) => {
+        localStorage.setItem("isAuthenticated", "true");
+        localStorage.setItem(username, password);
+    };
 
 
-  const handleCreateAccount = (e) => {
-    e.preventDefault();
-    if (username in storedUsers) {
-      setError("Username already exists. Try another one please ;)");
-    } else {
-      storedUsers[username] = password;
-      localStorage.setItem("users", JSON.stringify(storedUsers)); // Stringify to store in JSON
-      localStorage.setItem("isAuthenticated", "true");
-      localStorage.setItem("currentUser", username);
-    }
-  };
+    const handleCreateAccount = (e) => {
+        localStorage.setItem("isAuthenticated", "true");
+        localStorage.setItem(username, password);
+    };
 
-  return (
-    <main>
-    <div>
-        <h1><b>Welcome to JagarChat!</b></h1>
-        <form method="get" action="chat.html" className="loginForm">
-            <div className="input-group">
-                <span className="input-group-text">👤</span>
-                <input className="form-control" type="text"
-                    placeholder="Enter Username" value={username}
-                    onChange={(e) => setUsername(e.target.value)} required />
+    return (
+        <main>
+            <div>
+                <h1><b>Welcome to JagarChat!</b></h1>
+                <form method="get" action="chat.html" className="loginForm">
+                    <div className="input-group">
+                        <span className="input-group-text">👤</span>
+                        <input className="form-control" type="text"
+                            placeholder="Enter Username" value={username}
+                            onChange={(e) => setUsername(e.target.value)} required />
+                    </div>
+                    <div className="input-group">
+                        <span className="input-group-text">🔒</span>
+                        <input className="form-control" type="password"
+                            placeholder="Enter Password" value={password}
+                            onChange={(e) => setPassword(e.target.value)} required />
+                    </div>
+                    <button onClick={handleLogin} className="btn btn-secondary">Login</button>
+                    <button onClick={handleCreateAccount} className="btn btn-primary">Create</button>
+                </form>
             </div>
-            <div className="input-group">
-                <span className="input-group-text">🔒</span>
-                <input className="form-control" type="password"
-                    placeholder="Enter Password" value={password}
-                    onChange={(e) => setPassword(e.target.value)} required />
-            </div>
-            <button onClick={handleLogin} className="btn btn-secondary">Login</button>
-            <button onClick={handleCreateAccount} className="btn btn-primary">Create</button>
-        </form>
-    </div>
-    <br />
-</main>
-  );
+            <br />
+        </main>
+    );
 }
