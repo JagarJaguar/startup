728c462220950b5c8c72b3cf9cce912ec4447a7d
Ashton <thejagar22@gmail.com>
2025-02-26 16:48:55 -0700

    -Removed navlink import from login since not used anymore.

--- html/src/login/login.jsx
+++ html/src/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
     const [username, setUsername] = useState(localStorage.getItem("username") || "");
