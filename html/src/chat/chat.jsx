7035a7d94358d6793aa02d7d5adf40eed94441d1
Ashton <thejagar22@gmail.com>
2025-02-26 16:26:27 -0700

    -Set username in /chat to the actual username.

--- html/src/chat/chat.jsx
+++ html/src/chat/chat.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './chat.css';
 
 export function Chat() {
+    const username = localStorage.getItem("username"); 
+
     return (
         <main className="chat-page">
             <div className="chat-container">
                 <h2>Start Chatting Now!</h2>
-                <label htmlFor="textarea"><b>Logged in as: </b> Username</label>
+                <label htmlFor="textarea"><b>Logged in as: </b> {username}</label>
                 <br />
                 <textarea id="textarea" name="varTextarea" className="form-control" rows="14" cols="60"readOnly>
                     Melissa: How is HTML going so far?
