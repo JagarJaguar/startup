826132630cf7668a3f28edc0ce88695d889eb8ad
Ashton <thejagar22@gmail.com>
2025-04-03 02:41:51 -0600

    -Removed too much of chatProxy, brought back most of it from Simon.

--- html/service/chatProxy.js
+++ html/service/chatProxy.js
@@ -1,30 +1,49 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
 function chatProxy(httpServer) {
+  // Create a websocket object
   const socketServer = new WebSocketServer({ server: httpServer });
 
+  // Keep track of all the connections so we can forward messages
+  let connections = [];
+
   socketServer.on('connection', (socket) => {
-    socket.isAlive = true;
+    const connection = { id: uuid.v4(), alive: true, ws: socket };
+    connections.push(connection);
 
     socket.on('message', function message(data) {
-      socketServer.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: 'message_sent' }));
+      connections.forEach((c) => {
+        if (c.id !== connection.id && c.ws.readyState === WebSocket.OPEN) {
+          c.ws.send(JSON.stringify({ type: 'message_sent' }));
         }
       });
     });
 
+    // Respond to pong messages by marking the connection alive
     socket.on('pong', () => {
-      socket.isAlive = true;
+      connection.alive = true;
+    });
+
+    // Remove the closed connection so we don't try to forward anymore
+    socket.on('close', () => {
+      const pos = connections.findIndex((o, i) => o.id === connection.id);
+
+      if (pos >= 0) {
+        connections.splice(pos, 1);
+      }
     });
   });
 
   // Keep active connections alive
   const interval = setInterval(() => {
-    socketServer.clients.forEach((client) => {
-      if (!client.isAlive) {
-        return client.terminate();
+    connections.forEach((c) => {
+      // Kill any connection that didn't respond to the ping last time
+      if (!c.alive) {
+        c.ws.terminate();
       } else {
-        client.isAlive = false;
-        client.ping();
+        c.alive = false;
+        c.ws.ping();
       }
     });
   }, 10000);
@@ -33,3 +52,5 @@ function chatProxy(httpServer) {
     clearInterval(interval);
   });
 }
+
+module.exports = { chatProxy };
